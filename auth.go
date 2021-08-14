90088bc052027dcfb59ca7f55b2017531f5a8f56
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-14 16:41:57 -0700

    added server package, env setup.

--- auth.go
+++ auth.go
@@ -14,21 +14,26 @@ import (
 
 func main() {
 	fmt.Println("we out here lmao")
-
 	authApp()
 }
 
 // authorize app through spotify
 func authApp() {
 
+	// I. get client ID + client Secret from env.go
 	clientID := Auth().CLIENT_ID
 	clientSecret := Auth().CLIENT_SECRET
 
+	// II. create provider for spotify
 	goth.UseProviders(
 		spotify.New(clientID, clientSecret, "callbackurlidk"),
 	)
 
+	// III. ???
 	gothic.SetState = func(r *http.Request) string {
 		return r.URL.Query().Get("state")
 	}
+
+	// IV. Profit
+
 }
